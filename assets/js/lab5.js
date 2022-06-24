89355570a746defe03a5f26689083d12eddd4dc6
Alikhan Amandyk <alikhan.amandyk@dnapayments.com>
2022-06-24 14:20:05 -0500

    added semicolons

--- assets/js/lab5.js
+++ assets/js/lab5.js
@@ -1,13 +1,13 @@
 function sum(arr) {
     return arr
         .filter(x => x > 20)
-        .reduce((sum, x) => sum + x, 0)
+        .reduce((sum, x) => sum + x, 0);
 }
 
 function getNewArray(arr) {
     return arr
-        .filter(x => x.length > 5 && x.includes('a'))
+        .filter(x => x.length > 5 && x.includes('a'));
 }
 
-console.log(sum([1, 2, 3, 5, 21, 25]))
-console.log(getNewArray(['Alikhan', 'apple', 'ash', 'prior', 'appetite']))
+console.log(sum([1, 2, 3, 5, 21, 25]));
+console.log(getNewArray(['Alikhan', 'apple', 'ash', 'prior', 'appetite']));
