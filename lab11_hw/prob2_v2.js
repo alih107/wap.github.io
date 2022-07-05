a578ed38c657cd658cd5930710e3a49e506213bf
Alikhan Amandyk <alikhan.amandyk@dnapayments.com>
2022-07-05 14:26:25 -0500

    fixed bug

--- lab11_hw/prob2_v2.js
+++ lab11_hw/prob2_v2.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 http.createServer().on('request', (req, res) => {
     let stream = fs.createReadStream('big_image.jpg');
-    stream.on('on', () => {
+    stream.on('open', () => {
         stream.pipe(res);
     });
 }).listen(3000);
