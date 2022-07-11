6f9011c133fbb297dc72e79e5597ad6a9c496437
Alikhan Amandyk <alikhan.amandyk@dnapayments.com>
2022-07-11 00:44:47 -0500

    added checking if a user does exist
    Signed-off-by: Alikhan Amandyk <alikhan.amandyk@dnapayments.com>

--- project/server/middleware/authMiddleware.js
+++ project/server/middleware/authMiddleware.js
@@ -1,8 +1,9 @@
 const authUtils = require('../utils/auth')
+const User = require("../models/user");
 
 exports.checkAuth = (req, res, next) => {
     let username = authUtils.extractUser(req.headers.authorization);
-    if (username) {
+    if (User.doesUserExist(username)) {
         req.user = username;
         next();
     } else {
